166ce47d27665441c534fd50eba60170d472305e
diva.miet.cse <diva.miet.cse@gmail.com>
2017-05-28 22:08:24 +0530

    Added SortByValue and SparkContextManager

--- src/main/scala/com/dk/spark/algo/TopNForUniqueKeys.scala
+++ src/main/scala/com/dk/spark/algo/TopNForUniqueKeys.scala
@@ -6,7 +6,7 @@ import scala.collection.immutable.TreeMap
 /**
   * Created by Divakant Pandey on 5/28/17.
   */
-object TopN {
+object TopNForUniqueKeys {
 
   val N=10
   val sc = SparkContextManager.getContext()
@@ -16,17 +16,17 @@ object TopN {
     val input = sc.textFile("src/main/resources/topn.txt").filter(x => x != "")
     val pairs = input.map(x => {
       val t = x.split(",");
-      (t(0).toInt, t(1))
+      (t(1), t(0))
     })
     val partitions = pairs.mapPartitions(word => sortByPartition(word))
     partitions.collect().foreach(println)
   }
 
-  def sortByPartition(word:Iterator[(Int,String)]):Iterator[(Int,String)]={
-    var top10=new TreeMap[Int,String]
+  def sortByPartition(word: Iterator[(String, String)]): Iterator[(String, String)] = {
+    var top10 = new TreeMap[String, String]
     while(word.hasNext){
       val tup=word.next()
-      top10+=(tup._1->tup._2)
+      top10 += (tup._2 -> tup._1)
       if(top10.size>broadcastTopN.value){
         //
         top10-=top10.firstKey
